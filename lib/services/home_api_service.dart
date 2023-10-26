aedcd748d0e7bfa34e7557be7ace7dd23c934d86
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2023-10-26 10:40:57 +0100

    updated

--- lib/services/home_api_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'api_service.dart';
-
-class HomeApiService extends ApiService {
-  static String homeUrl = '/jsonapi/node/article';
-  Future<Response<dynamic>> loadHome() async {
-    return post(homeUrl, {}, contentType: 'application/vnd.api+json');
-  }
-}
